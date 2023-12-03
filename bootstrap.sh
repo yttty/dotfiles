a3ce7eb611eabd9ea31be022db9705f3c5b5cf38
yttty <yttty@noreply.com>
2023-12-03 22:53:20 +0800

    fix: bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,16 @@
 #! /bin/sh
 
-# 1. nvm, nodejs, and diff-so-fancy
+# 1. zsh and oh-my-zsh config
+git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+curl -fLo ~/.zshrc https://raw.githubusercontent.com/yttty/dotfiles/main/.zshrc
+
+# 2. git config
+curl -fLo ~/.gitignore https://raw.githubusercontent.com/yttty/dotfiles/main/.gitignore
+curl -fLo ~/.gitconfig https://raw.githubusercontent.com/yttty/dotfiles/main/.gitconfig
+
+# 3. nvm, nodejs, and diff-so-fancy
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -8,19 +18,9 @@ nvm install node
 nvm use node
 npm install -g diff-so-fancy
 
-# 2. git config
-curl -fLo ~/.gitignore https://raw.githubusercontent.com/yttty/dotfiles/master/.gitignore
-curl -fLo ~/.gitconfig https://raw.githubusercontent.com/yttty/dotfiles/master/.gitconfig
-
-# 3. zsh oh-my-zsh config
-git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-curl -fLo ~/.zshrc https://raw.githubusercontent.com/yttty/dotfiles/master/.zshrc
-
 # 4. tmux config
-curl -fLo ~/.tmux.conf https://raw.githubusercontent.com/yttty/dotfiles/master/.tmux.conf
+curl -fLo ~/.tmux.conf https://raw.githubusercontent.com/yttty/dotfiles/main/.tmux.conf
 
 # 5. vim config
-curl -fLo ~/.vimrc https://raw.githubusercontent.com/yttty/dotfiles/master/.vimrc
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/yttty/dotfiles/main/.vimrc
 echo | vim -c "PlugInstall! | qall! "
